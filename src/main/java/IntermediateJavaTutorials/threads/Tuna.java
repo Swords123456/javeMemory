cba8fa9ace080c1fab38fbf758f30add6e5635ee
Swords123456 <33922797+Swords123456@users.noreply.github.com>
2018-07-08 22:14:47 +1000

    Threads

---
+++ src/main/java/IntermediateJavaTutorials/threads/Tuna.java
@@ -0,0 +1,24 @@
+package IntermediateJavaTutorials.threads;
+
+import java.util.Random;
+
+public class Tuna implements Runnable{
+    String name;
+    int time;
+    Random r = new Random();
+    public Tuna(String x){
+        name = x;
+        time = r.nextInt(999);
+    }
+
+    @Override
+    public void run() {
+        try{
+            System.out.printf("%s us sleeping for %d,\n", name, time);
+            Thread.sleep(time);
+            System.out.printf("%s is done\n", name);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
